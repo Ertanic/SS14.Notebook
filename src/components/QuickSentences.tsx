bf966f188670837242758b9d99c86d09948d7e9a
Ertanic <black.ikra.14@gmail.com>
2024-02-10 22:57:37 +0300

    Ability to create sentences has been added

--- src/components/QuickSentences.tsx
+++ src/components/QuickSentences.tsx
@@ -3,6 +3,10 @@ import { QuickSentence, QuickSentenceCategory, QuickSentenceCategoryRepository, 
 import { createStore } from "solid-js/store";
 import styles from './QuickSentences.module.scss';
 import trash_icon from '../assets/trash_icon.svg';
+import add_icon from '../assets/add_icon.svg';
+import check_icon from '../assets/check_icon.svg';
+import Modal from "@lutaok/solid-modal";
+import CopyButton from "./CopyButton";
 
 function QuickSentences() {
     type QuickSentenceState = { categories: QuickSentenceCategory[], sentences: QuickSentence[] };
@@ -30,15 +34,38 @@ function QuickSentences() {
 
     const enteredCategory = (title: string) => {
         const newCategory = new QuickSentenceCategory(title);
-        categoriesRepo.Insert([newCategory]);
+        createEffect(async () => await categoriesRepo.Insert([newCategory]));
         setState('categories', (c) => [...c, newCategory]);
     }
 
     const deletingCategory = (id: number) => {
-        categoriesRepo.Delete(id);
         setState('categories', c => [...c.filter(i => i.category_id !== id)]);
+        createEffect(async () => {
+            await categoriesRepo.Delete(id);
+            const sentences = state.sentences.filter(s => s.category_id === id);
+            for (const sentence of sentences) {
+                await sentenceRepo.Delete(sentence.quick_sentence_id);
+            }
+        })
+    }
 
-        console.log(state);
+    const [isOpenModal, setIsOpenModal] = createSignal(false);
+    const [categoryId, setCategoryId] = createSignal<number>(0);
+    const addSentence = (id: number) => {
+        setCategoryId(id);
+        setIsOpenModal(true);
+    };
+
+    const addingSentence = (e: SubmitEvent) => {
+        e.preventDefault();
+        setIsOpenModal(false);
+
+        const form = (e.currentTarget as HTMLFormElement);
+        const [title, sentence] = [(form.elements[0] as HTMLInputElement).value, (form.elements[1] as HTMLInputElement).value];
+
+        const model = new QuickSentence(title, sentence, categoryId());
+        setState('sentences', s => [...s, model]);
+        createEffect(async () => await sentenceRepo.Insert([model]));
     }
 
     return (
@@ -46,28 +73,64 @@ function QuickSentences() {
             <h1>Быстрое копирование фраз</h1>
             <div class={styles.categories}>
                 <For each={state.categories}>
-                    {item => <Category item={item} onDeleteCategory={deletingCategory} />}
+                    {item => 
+                        <Category 
+                            item={item} 
+                            sentences={state.sentences.filter(s => s.category_id === item.category_id)} 
+                            onDeleteCategory={deletingCategory} 
+                            onAddSentence={addSentence} />}
                 </For>
             </div>
             <AddCategory onEnterCategory={enteredCategory} />
+
+            <Modal contentClass={styles.modal} overlayClass={styles.overlay} closeOnOutsideClick isOpen={isOpenModal()} onCloseRequest={() => setIsOpenModal(false)}>
+                <form onSubmit={addingSentence}>
+                    <div class={styles.modal__form_row}>
+                        <div class={styles.modal__form_column}>
+                            <label for="sentence_title">Название</label>
+                            <label for="sentence_content">Фраза</label>
+                        </div>
+                        <div class={styles.modal__form_column}>
+                            <input type="text" maxlength={30} id="sentence_title" name="title" />
+                            <textarea wrap="hard" name="sentence" id="sentence_content" rows={5} />
+                        </div>
+                    </div>
+                    <button class={styles.check_button} type="submit"><img src={check_icon} /></button>
+                </form>
+            </Modal>
         </section>
     );
 }
 
-// TODO: Add button for new sentence
-type AddingSentenceHandler = () => void;
+type AddingSentenceHandler = (category_id: number) => void;
 type DeletingCategoriesHandler = (category_id: number) => void;
-type CategoryProps = { item: QuickSentenceCategory, onAddSentence?: AddingSentenceHandler, onDeleteCategory?: DeletingCategoriesHandler };
+type CategoryProps = {
+    item: QuickSentenceCategory,
+    sentences: Array<QuickSentence>,
+    onAddSentence?: AddingSentenceHandler,
+    onDeleteCategory?: DeletingCategoriesHandler
+};
 function Category(props: CategoryProps) {
     const clickedTrashButton = () => {
         if (props.onDeleteCategory) props.onDeleteCategory(props.item.category_id);
     };
+    const clickedAddButton = () => {
+        if (props.onAddSentence) props.onAddSentence(props.item.category_id);
+    }
 
     return (
         <div class={styles.category}>
             <div class={styles.category_title}>
                 <h2>{props.item.name}</h2>
-                <div class={styles.trash_icon} onClick={clickedTrashButton}><img src={trash_icon} /></div>
+                <div class={styles.icons_container}>
+                    <div class={styles.icon} onClick={clickedAddButton}><img src={add_icon} /></div>
+                    <div class={styles.icon} onClick={clickedTrashButton}><img src={trash_icon} /></div>
+                </div>
+            </div>
+            <div class={styles.category_buttons}>
+                <For each={props.sentences}>
+                    {item => <CopyButton content={item.sentence}> {item.title} </CopyButton>}
+                </For>
             </div>
         </div>
     )
